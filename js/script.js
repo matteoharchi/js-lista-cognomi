603a67a0fb18e382288b3aa515c3a500c22c2682
Matteo Harchi <matteo.harchi@gmail.com>
2020-07-13 15:49:27 +0200

    Finito

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Variabili fisse
+var iscritti = [ 'Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var newclient = prompt("Inserisci il tuo cognome");
+var client;
+// Sistemo maiuscole e minuscole
+var clientsurname = primoCarattere + altricaratteri;
+var primoCarattere = newclient.charAt(0).toUpperCase(newclient);
+var altricaratteri = newclient.slice(1).toLowerCase(newclient);
+// Inserisco i cognomi all'interno della lista
+// in ordine alfabetico con sort, che ha a-z come default.
+iscritti.push(clientsurname);
+iscritti.sort();
+// in un ciclo for, per comodità, creo una variabile client
+// per gestire ogni singolo elemento del array e la stampo
+// su li.
+for (var i = 0; i < iscritti.length; i++) {
+    client = iscritti [i];
+    document.getElementById("clientslist").innerHTML += "<li>" + client + "</li>";
+}
