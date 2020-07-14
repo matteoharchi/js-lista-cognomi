a5513a332137bec4b0f0894f09f5684920c9b707
Matteo Harchi <matteo.harchi@gmail.com>
2020-07-14 09:59:06 +0200

    Risistemato casino fatto riordinando, ora funziona di nuovo

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var iscritti = [ 'Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
 var newclient = prompt("Inserisci il tuo cognome");
 var client;
 // Sistemo maiuscole e minuscole
-var clientsurname = primoCarattere + altricaratteri;
 var primoCarattere = newclient.charAt(0).toUpperCase(newclient);
 var altricaratteri = newclient.slice(1).toLowerCase(newclient);
+var clientsurname = primoCarattere + altricaratteri;
 // Inserisco i cognomi all'interno della lista
 // in ordine alfabetico con sort, che ha a-z come default.
 iscritti.push(clientsurname);
@@ -14,6 +14,6 @@ iscritti.sort();
 // per gestire ogni singolo elemento del array e la stampo
 // su li.
 for (var i = 0; i < iscritti.length; i++) {
-    client = iscritti [i];
+    client = iscritti[i];
     document.getElementById("clientslist").innerHTML += "<li>" + client + "</li>";
 }
